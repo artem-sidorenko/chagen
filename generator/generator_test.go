4157298ef9056eb02c13ae0345428e5916f5f3b6
Artem Sidorenko <artem@posteo.de>
2017-04-10 20:01:25 +0200

    Basic template rendering with issues

---
+++ generator/generator_test.go
@@ -0,0 +1,105 @@
+/*
+   Copyright 2017 Artem Sidorenko <artem@posteo.de>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package generator
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/artem-sidorenko/chagen/data"
+)
+
+const fullGeneratedChangelog = `
+Changelog
+=========
+
+## [v0.1.0](https://example.com/release/v0.1.0) (2017-04-11)
+
+Closed issues
+-------------
+- Test issue of new release [\#10](https://example.com/issue/10)
+
+## [v0.0.1](https://example.com/release/v0.0.1) (2017-04-10)
+
+Closed issues
+-------------
+- Test issue [\#1](https://example.com/issue/1)
+
+`
+
+var fullDataStructure = []data.Release{
+	{
+		Release:    "v0.1.0",
+		ReleaseURL: "https://example.com/release/v0.1.0",
+		Date:       "2017-04-11",
+		Issues: []data.Issue{
+			{
+				Name: "Test issue of new release",
+				ID:   "10",
+				URL:  "https://example.com/issue/10",
+			},
+		},
+	},
+	{
+		Release:    "v0.0.1",
+		ReleaseURL: "https://example.com/release/v0.0.1",
+		Date:       "2017-04-10",
+		Issues: []data.Issue{
+			{
+				Name: "Test issue",
+				ID:   "1",
+				URL:  "https://example.com/issue/1",
+			},
+		},
+	},
+}
+
+func TestGenerator_Render(t *testing.T) {
+	type fields struct {
+		Releases []data.Release
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantWr  string
+		wantErr bool
+	}{
+		{
+			name: "verify testdata structure",
+			fields: fields{
+				Releases: fullDataStructure,
+			},
+			wantWr:  fullGeneratedChangelog,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &Generator{
+				Releases: tt.fields.Releases,
+			}
+			wr := &bytes.Buffer{}
+			if err := g.Render(wr); (err != nil) != tt.wantErr {
+				t.Errorf("Generator.Render() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotWr := wr.String(); gotWr != tt.wantWr {
+				t.Errorf("Generator.Render() = %v, want %v", gotWr, tt.wantWr)
+			}
+		})
+	}
+}
