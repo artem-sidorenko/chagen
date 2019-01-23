d21438494dd0722c1d13dc496ae1f60fb85084c1
Artem Sidorenko <artem@posteo.de>
2019-01-23 17:43:17 +0100

    Refactoring: move out helper functions
    Signed-off-by: Artem Sidorenko <artem@posteo.de>

--- connectors/github/github.go
+++ connectors/github/github.go
@@ -21,9 +21,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net/url"
 	"os"
-	"path"
 
 	"github.com/artem-sidorenko/chagen/connectors"
 	"github.com/artem-sidorenko/chagen/connectors/github/internal/client"
@@ -49,11 +47,6 @@ type Connector struct {
 // NewGitHubClientFunc links to the constructor, which is used to create Connector.client
 var NewGitHubClientFunc = client.NewGitHubClient // nolint: gochecknoglobals
 
-// formatErrorCode formats the error message for this connector
-func formatErrorCode(query string, err error) error {
-	return fmt.Errorf("GitHub query '%s' failed: %s", query, err)
-}
-
 // RepositoryExists checks if referenced repository is present
 func (c *Connector) RepositoryExists() (bool, error) {
 	_, resp, err := c.client.Repositories.Get(c.context, c.Owner, c.Repo)
@@ -72,44 +65,6 @@ func (c *Connector) RepositoryExists() (bool, error) {
 			fmt.Errorf("unhandled HTTP response code %v", resp.StatusCode),
 		)
 	}
-}
-
-// GetNewTagURL returns the URL for a new tag, which does not exist yet
-func (c *Connector) GetNewTagURL(TagName string) (string, error) {
-	return c.getTagURL(TagName, c.NewTagUseReleaseURL)
-}
-
-// getTagURL returns the URL for a given tag.
-// If alwaysUseReleaseURL is true: URL is provided for release page,
-// even if it does not exist yet
-func (c *Connector) getTagURL(tagName string, alwaysUseReleaseURL bool) (string, error) {
-	release, resp, err := c.client.Repositories.GetReleaseByTag(c.context, c.Owner, c.Repo, tagName)
-	if err != nil {
-		// no release was found for this tag, this is no error for us
-		if resp.StatusCode != 404 {
-			return "", formatErrorCode("getTagURL", err)
-		}
-	}
-
-	// if GitHub release for this tag was found -> use it
-	// generate otherwise a link to the git tag view in the file tree
-	var tagURL string
-	if release != nil { // we got real release URL, use it
-		tagURL = release.GetHTMLURL()
-	} else { // build own URL
-		u, err := url.Parse(c.ProjectURL)
-		if err != nil {
-			return "", err
-		}
-
-		if alwaysUseReleaseURL { // try to build own release url
-			u.Path = path.Join(u.Path, "/releases/"+tagName)
-		} else { // build tag url
-			u.Path = path.Join(u.Path, "/tree/"+tagName)
-		}
-		tagURL = u.String()
-	}
-	return tagURL, nil
 }
 
 // GetTags returns the git tags
---
+++ connectors/github/helpers.go
@@ -0,0 +1,66 @@
+/*
+   Copyright 2019 Artem Sidorenko <artem@posteo.de>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package github
+
+import (
+	"fmt"
+	"net/url"
+	"path"
+)
+
+// formatErrorCode formats the error message for this connector
+func formatErrorCode(query string, err error) error {
+	return fmt.Errorf("GitHub query '%s' failed: %s", query, err)
+}
+
+// getTagURL returns the URL for a given tag.
+// If alwaysUseReleaseURL is true: URL is provided for release page,
+// even if it does not exist yet
+func (c *Connector) getTagURL(tagName string, alwaysUseReleaseURL bool) (string, error) {
+	release, resp, err := c.client.Repositories.GetReleaseByTag(c.context, c.Owner, c.Repo, tagName)
+	if err != nil {
+		// no release was found for this tag, this is no error for us
+		if resp.StatusCode != 404 {
+			return "", formatErrorCode("getTagURL", err)
+		}
+	}
+
+	// if GitHub release for this tag was found -> use it
+	// generate otherwise a link to the git tag view in the file tree
+	var tagURL string
+	if release != nil { // we got real release URL, use it
+		tagURL = release.GetHTMLURL()
+	} else { // build own URL
+		u, err := url.Parse(c.ProjectURL)
+		if err != nil {
+			return "", err
+		}
+
+		if alwaysUseReleaseURL { // try to build own release url
+			u.Path = path.Join(u.Path, "/releases/"+tagName)
+		} else { // build tag url
+			u.Path = path.Join(u.Path, "/tree/"+tagName)
+		}
+		tagURL = u.String()
+	}
+	return tagURL, nil
+}
+
+// GetNewTagURL returns the URL for a new tag, which does not exist yet
+func (c *Connector) GetNewTagURL(TagName string) (string, error) {
+	return c.getTagURL(TagName, c.NewTagUseReleaseURL)
+}
